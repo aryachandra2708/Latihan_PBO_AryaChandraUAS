251adc557038274d15a2198c36984487a37ebb7f
perasperaadastra27 <aryabowo333@gmail.com>
2026-06-18 21:09:14 +0700

    [Tahap 3-6]

--- classes/pendaftaran_kedinasan.php
+++ classes/pendaftaran_kedinasan.php
@@ -11,7 +11,7 @@ class PendaftaranKedinasan extends Pendaftaran {
         $this->instansi_sponsor = $sponsor;
     }
 
-    // Polimorfisme Overriding (Tahap 5)
+    // Tahap 5: Overriding
     public function hitungTotalBiaya() {
         return $this->biaya_pendaftaran_dasar * 1.25;
     }
@@ -20,12 +20,11 @@ class PendaftaranKedinasan extends Pendaftaran {
         return "Sponsor: " . $this->instansi_sponsor . " (SK: " . $this->sk_ikatan_dinas . ")";
     }
 
-    // Query Spesifik (Tahap 4)
+    // Tahap 4: Metode ini yang kamu butuhkan (tambahkan ini ke dalam class!)
     public static function getDaftarKedinasan($db) {
         $query = "SELECT * FROM tabel_pendaftaran WHERE jalur_pendaftaran = 'Kedinasan'";
         $stmt = $db->prepare($query);
         $stmt->execute();
-
         $result = [];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $result[] = new self($row['id_pendaftaran'], $row['nama_calon'], $row['asal_sekolah'], $row['nilai_ujian'], $row['biaya_pendaftaran_dasar'], $row['sk_ikatan_dinas'], $row['instansi_sponsor']);
